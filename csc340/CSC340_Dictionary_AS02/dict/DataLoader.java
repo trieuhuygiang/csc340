f878ccf07f401bad10191e31158763d6d96825fb
trieuhuygiang <trieuhuygiang@gmail.com>
2026-02-09 18:46:39 -0800

    redo it

--- csc340/CSC340_Dictionary_AS02/dict/DataLoader.java
+++
@@ -1,9 +0,0 @@
-import java.util.Arrays;
-
-public class DataLoader {
-    public static Dictionary load() {
-        Dictionary dict = new Dictionary();
-        dict.loadAll(Arrays.asList(DataSource.values()));
-        return dict;
-    }
-}
